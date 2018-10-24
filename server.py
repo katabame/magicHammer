d00360b262e9ec17c7b81b7686c4949b2832ef3a
Katabame Ayame <katabamia@gmail.com>
2018-10-25 01:43:53 +0900

    Add: procfile

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def index():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	connectionHandler = threading.Thread(target = app.run, daemon = True)
+	connectionHandler = threading.Thread(target = app.run(debug=False, host='0.0.0.0', port=80), daemon = True)
 	connectionHandler.start()
 	while True:
 		try:
