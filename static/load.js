757d408d5822ba1b20bbfbb66095950b7085f702
azure <rz7@outlook.jp>
2019-07-16 06:48:23 +0900

    Separate requests for faster loading

--- static/load.js
+++ static/load.js
@@ -1,22 +1,21 @@
 function request()
 {
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function()
-	{
-		if(req.readyState == 4 && req.status == 200)
+	["stable", "ptb", "canary"].forEach(channel => {
+		let req = new XMLHttpRequest();
+		req.onreadystatechange = function()
 		{
-			var json = JSON.parse(req.responseText);
-			console.log(json);
-			json.forEach(data =>
+			if(req.readyState == 4 && req.status == 200)
 			{
-				element = document.getElementById(data['release_channel']);
-				element.getElementsByClassName('build_number')[0].textContent = data['build_number'];
-				element.getElementsByClassName('version_hash')[0].textContent = data['version_hash'];
-			});
-		}
-	};
-	req.open("GET", "api/all.json", true);
-	req.send();
+				var json = JSON.parse(req.responseText);
+				console.log(json);
+				element = document.getElementById(json['release_channel']);
+				element.getElementsByClassName('build_number')[0].textContent = json['build_number'];
+				element.getElementsByClassName('version_hash')[0].textContent = json['version_hash'];
+			}
+		};
+		req.open("GET", `api/${channel}.json`, true);
+		req.send();
+	});
 }
 
 window.onload = request();
