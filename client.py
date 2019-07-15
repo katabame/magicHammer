ea9d0228a98f15d5cbff8947c8fd68b0b95798dd
azure <rz7@outlook.jp>
2019-07-16 04:48:10 +0900

    PEP 8

--- client.py
+++ client.py
@@ -10,88 +10,101 @@ from concurrent.futures import Future, TimeoutError
 from pyppeteer import launch
 from enum import Enum
 
+
 class DiscordVersionClient:
-	"""
-	DiscordVersionClient class represents an interface of version fetcher.
-	"""
-	def get(self, taskQueue: queue):
-		pass
+    """
+    DiscordVersionClient class represents an interface of version fetcher.
+    """
+
+    def get(self, taskQueue: queue):
+        pass
+
 
 class CachedDiscordVersionClient(DiscordVersionClient):
-	DEFAULT_TIMEOUT = timedelta(minutes = 1)
+    DEFAULT_TIMEOUT = timedelta(minutes=1)
 
-	def __init__(self, delegation: DiscordVersionClient, timeout: timedelta = DEFAULT_TIMEOUT):
-		self.delegation = delegation
-		self.timeout = timeout
-		self.lastUpdated = datetime.now() - timeout
-		self.cached = None
+    def __init__(self, delegation: DiscordVersionClient, timeout: timedelta = DEFAULT_TIMEOUT):
+        self.delegation = delegation
+        self.timeout = timeout
+        self.lastUpdated = datetime.now() - timeout
+        self.cached = None
 
-	def get(self, taskQueue: queue.Queue):
-		currentTime = datetime.now()
-		if (currentTime - self.lastUpdated) > self.timeout:
-			self.lastUpdated = currentTime
-			self.cached = self.delegation.get(taskQueue)
-		return self.cached
+    def get(self, taskQueue: queue.Queue):
+        currentTime = datetime.now()
+        if (currentTime - self.lastUpdated) > self.timeout:
+            self.lastUpdated = currentTime
+            self.cached = self.delegation.get(taskQueue)
+        return self.cached
+
 
 class SimpleDiscordVersionClient(DiscordVersionClient):
-	"""
-	SimpleDiscordVersionClient class implements DiscordVersionClient interface.
-	Method "get" fetches version data from discordapp.com every time.
-	For performance, use CachedDiscordVersionClient.
-	"""
+    """
+    SimpleDiscordVersionClient class implements DiscordVersionClient interface.
+    Method "get" fetches version data from discordapp.com every time.
+    For performance, use CachedDiscordVersionClient.
+    """
 
-	def __init__(self, url: str):
-		self.url = url
+    def __init__(self, url: str):
+        self.url = url
 
-	def get(self, taskQueue: queue.Queue):
-		return self.request(taskQueue)
+    def get(self, taskQueue: queue.Queue):
+        return self.request(taskQueue)
 
-	def onConsoleMessage(self, msg: str):
-		log = [line[line.rfind(' ') + 1:] for line in msg.text.split(',')]
-		return { 'release_channel': log[0], 'build_number': log[1], 'version_hash': log[2] }
+    def onConsoleMessage(self, msg: str):
+        log = [line[line.rfind(' ') + 1:] for line in msg.text.split(',')]
+        return {'release_channel': log[0], 'build_number': log[1], 'version_hash': log[2]}
 
-	def request(self, taskQueue: queue.Queue):
-		future = Future()
-		async def requestAsync(self, future):
-			browser = await launch(headless=True)
-			page = await browser.newPage()
+    def request(self, taskQueue: queue.Queue):
+        future = Future()
 
-			page.once('console', lambda msg: future.set_result(self.onConsoleMessage(msg))) 
-			
-			await page.goto(self.url)
-			await browser.close()
-		while True:
-			try:
-				taskQueue.put(requestAsync(self, future), block = False)
-				break
-			except queue.Full:
-				time.sleep(1)
-		while True:
-			try:
-				return future.result(timeout = 1)
-			except TimeoutError:
-				time.sleep(1)
+        async def requestAsync(self, future):
+            browser = await launch(headless=True)
+            page = await browser.newPage()
+
+            page.once('console', lambda msg: future.set_result(
+                self.onConsoleMessage(msg)))
+
+            await page.goto(self.url)
+            await browser.close()
+        while True:
+            try:
+                taskQueue.put(requestAsync(self, future), block=False)
+                break
+            except queue.Full:
+                time.sleep(1)
+        while True:
+            try:
+                return future.result(timeout=1)
+            except TimeoutError:
+                time.sleep(1)
 
 
 class DefaultDiscordVersionClients:
-	STABLE_NOCACHE	= SimpleDiscordVersionClient('https://discordapp.com/login')
-	PTB_NOCACHE		= SimpleDiscordVersionClient('https://ptb.discordapp.com/login')
-	CANARY_NOCACHE	= SimpleDiscordVersionClient('https://canary.discordapp.com/login')
-	STABLE	= CachedDiscordVersionClient(STABLE_NOCACHE)
-	PTB		= CachedDiscordVersionClient(PTB_NOCACHE)
-	CANARY	= CachedDiscordVersionClient(CANARY_NOCACHE)
+    STABLE_NOCACHE = SimpleDiscordVersionClient('https://discordapp.com/login')
+    PTB_NOCACHE = SimpleDiscordVersionClient(
+        'https://ptb.discordapp.com/login')
+    CANARY_NOCACHE = SimpleDiscordVersionClient(
+        'https://canary.discordapp.com/login')
+    STABLE = CachedDiscordVersionClient(STABLE_NOCACHE)
+    PTB = CachedDiscordVersionClient(PTB_NOCACHE)
+    CANARY = CachedDiscordVersionClient(CANARY_NOCACHE)
+
 
 def testTemplate(self, target):
-	self.assertNotEqual(isinstance(target.value, DefaultDiscordVersionClients))
-	self.assertEqual(isinstance(target.value, DiscordVersionClient))
+    self.assertNotEqual(isinstance(target.value, DefaultDiscordVersionClients))
+    self.assertEqual(isinstance(target.value, DiscordVersionClient))
+
 
 class DefaultDiscordVersionClientsTest(unittest.TestCase):
-	def testStable(self):
-		testTemplate(self, DefaultDiscordVersionClients.STABLE)
-	def testPTB(self):
-		testTemplate(self, DefaultDiscordVersionClients.PTB)
-	def testCanary(self):
-		testTemplate(self, DefaultDiscordVersionClients.CANARY)
+    def testStable(self):
+        testTemplate(self, DefaultDiscordVersionClients.STABLE)
+
+    def testPTB(self):
+        testTemplate(self, DefaultDiscordVersionClients.PTB)
+
+    def testCanary(self):
+        testTemplate(self, DefaultDiscordVersionClients.CANARY)
+
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
--- server.py
+++ server.py
@@ -11,41 +11,48 @@ from client import DiscordVersionClient, DefaultDiscordVersionClients, SimpleDis
 app = Flask(__name__)
 taskQueue = queue.Queue()
 
+
 def fetch(endpoint: DiscordVersionClient):
-	assert isinstance(endpoint, DiscordVersionClient)
-	return jsonify(endpoint.get(taskQueue))
+    assert isinstance(endpoint, DiscordVersionClient)
+    return jsonify(endpoint.get(taskQueue))
+
 
 @app.route('/api/stable.json', methods=['GET'])
 def fetchStable():
-	return fetch(DefaultDiscordVersionClients.STABLE)
-	
+    return fetch(DefaultDiscordVersionClients.STABLE)
+
+
 @app.route('/api/ptb.json', methods=['GET'])
 def fetchPTB():
-	return fetch(DefaultDiscordVersionClients.PTB)
+    return fetch(DefaultDiscordVersionClients.PTB)
+
 
 @app.route('/api/canary.json', methods=['GET'])
 def fetchCanary():
-	return fetch(DefaultDiscordVersionClients.CANARY)
+    return fetch(DefaultDiscordVersionClients.CANARY)
+
 
 @app.route('/api/all.json', methods=['GET'])
 def fetchAll():
-	return jsonify([DefaultDiscordVersionClients.STABLE.get(taskQueue), DefaultDiscordVersionClients.PTB.get(taskQueue), DefaultDiscordVersionClients.CANARY.get(taskQueue)])
+    return jsonify([DefaultDiscordVersionClients.STABLE.get(taskQueue), DefaultDiscordVersionClients.PTB.get(taskQueue), DefaultDiscordVersionClients.CANARY.get(taskQueue)])
+
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", "5000"))
-	connectionHandler = threading.Thread(target = app.run(debug=False, host='0.0.0.0', port=port), daemon = True)
-	connectionHandler.start()
-	while True:
-		try:
-			task = taskQueue.get(block = False)
-			if task:
-				loop = asyncio.new_event_loop()
-				loop.run_until_complete(task)
-				loop.close()
-		except queue.Empty:
-			time.sleep(1)
-
+    port = int(os.environ.get("PORT", "5000"))
+    connectionHandler = threading.Thread(target=app.run(
+        debug=False, host='0.0.0.0', port=port), daemon=True)
+    connectionHandler.start()
+    while True:
+        try:
+            task = taskQueue.get(block=False)
+            if task:
+                loop = asyncio.new_event_loop()
+                loop.run_until_complete(task)
+                loop.close()
+        except queue.Empty:
+            time.sleep(1)
